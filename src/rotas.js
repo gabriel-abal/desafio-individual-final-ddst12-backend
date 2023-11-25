4015ffdd98cda25f67bd11f9398fe96d8edc4834
Gabriel Abal <gabriel.maia.abal@gmail.com>
2023-11-25 19:40:31 -0300

    feat: Criado a rota de login

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { cadastrarUsuario } = require('./controladores/usuarios');
+const { login } = require('./controladores/login');
 
 const rotas = express();
 
 rotas.post('/usuario', cadastrarUsuario);
+rotas.post('/login', login)
 
 module.exports = rotas
